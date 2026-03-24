58451637829077c24598b34e627a99fad70d6ca7
theonewhoknocks51 <ghoshtitas26@gmail.com>
2026-03-24 09:04:34 +0530

    UC1Use Case 1: Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        // Print welcome message to the console
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Application Name: Book My Stay");
+        System.out.println("Version: 1.0");
+
+        // Program terminates after printing messages
+    }
+
+}
